1e9f0aa62280227e2f43a4bdbeeca4b48ec831e6
Keiko Kaneko <keiko.cda@gmail.com>
2022-04-09 17:52:52 +0900

    Update dotfilesLink.sh

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
-ln -sf ~/dotfiles/fish ~/config.fish
 ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
+
+mkdir ~/.config/fish ~/.config/fish/functions
+ln -sf ~/dotfiles/fish/config.fish ~/.config/fish/config.fish
+ln -sf ~/dotfiles/fish/functions/fish_prompt.fish ~/.config/fish/functions/fish_prompt.fish
+ln -sf ~/dotfiles/fish/fish_variables ~/.config/fish/fish_variables
