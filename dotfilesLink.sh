dc6d915161d48eced1b7135476258bdc9986d2f2
Keiko Kaneko <keiko.cda@gmail.com>
2022-09-04 12:39:58 +0900

    Copy tigrc from source repo

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -5,3 +5,4 @@ ln -sf ~/dotfiles/zsh/abbreviations ~/.config/zsh/abbreviations
 ln -sf ~/dotfiles/zsh/.zshenv  ~/.zshenv
 ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
 ln -sf ~/dotfiles/.git-prompt.sh ~/.git-prompt.sh
+ln -sf ~/dotfiles/.tigrc ~/.tigrc
